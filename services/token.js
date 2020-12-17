da2ee96c9c4e58c6a866d8fff79244700b0b9d89
imcrisam <imcrisam@gmail.com>
2020-12-17 09:56:41 -0500

    __id to id problemas en calificacion del reto

--- services/token.js
+++ services/token.js
@@ -2,25 +2,25 @@ var jwt = require('jsonwebtoken');
 const models = require('../models');
 
 async function checkToken(token) {
-    let __id = null;
+    let id = null;
     try {
         const {
             id
         } = await jwt.decode(token);
-        __id = id;
+        id = id;
     } catch (e) {
         return false;
     }
-    console.log(__id);
+    console.log(id);
     const user = await models.Usuario.findOne({
         where: {
-            id: __id,
+            id: id,
             estado: 1
         }
     });
     if (user) {
         const token = jwt.sign({
-            id: __id
+            id: id
         }, 'secretKeyToGenerateToken', {
             expiresIn: '1d'
         });
@@ -36,10 +36,10 @@ async function checkToken(token) {
 module.exports = {
 
     //generar el token
-    encode: async (_id, rol) => {
+    encode: async (id, rol) => {
         console.log(rol);
         const token = jwt.sign({
-            id: _id,
+            id: id,
             rol: rol
         }, 'secretKeyToGenerateToken', {
             expiresIn: '1d'
